17da7688c40622f93bae515df18d94d7d6d91a6d
Tomasz Terlecki <tomaszjerzyt@gmail.com>
2021-03-11 09:36:32 +0100

    zadziala?

---
+++ src/main/java/KeyboardReader.java
@@ -0,0 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class KeyboardReader {
+    private Scanner sc = new Scanner(System.in);
+
+    public void close() {
+        sc.close();
+    }
+
+    static int readInt(int range) {
+
+        boolean choice = true;
+        int result = -1;
+
+        while (choice) {
+            try {
+                result = sc.nextInt();
+                if (result>0 && result<=range) {
+                    choice = false;
+                } else {
+                    System.out.println("Podaj liczbę całkowitą pomiędzy 1-"+range);
+                }
+
+            } catch (InputMismatchException ex) {
+                System.out.println("Podaj liczbę całkowitą pomiędzy 1-"+range);
+                sc.next();
+            }
+
+        }
+
+        return result;
+
+    }
+    public String readString() {
+
+        String result= sc.nextLine();
+
+        return result;
+    }
+
+}
